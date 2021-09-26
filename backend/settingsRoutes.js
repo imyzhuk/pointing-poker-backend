9058c3ef3bf4b6014130d0337b5a1e2d0b0bd158
imyzhuk <imyzhuk@gmail.com>
2021-09-26 10:23:18 +0300

    refactor: replace double quote to single quote

--- backend/settingsRoutes.js
+++ backend/settingsRoutes.js
@@ -1,14 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Game = require("./game");
+const Game = require('./game');
 
-router.post("/:gameId", async (req, res) => {
+router.post('/:gameId', async (req, res) => {
   const {
     settings: { isOwnerAPlayer, ...restSettings },
   } = req.body;
   try {
     const game = await Game.findOne({ id: req.params.gameId });
-    game.members[0]["userRole"] = isOwnerAPlayer ? "player" : "observer";
+    game.members[0]['userRole'] = isOwnerAPlayer ? 'player' : 'observer';
     game.settings = restSettings;
     await game.save();
     res.sendStatus(200);
@@ -18,7 +18,7 @@ router.post("/:gameId", async (req, res) => {
   }
 });
 
-router.get("/:gameId", async (req, res) => {
+router.get('/:gameId', async (req, res) => {
   try {
     const game = await Game.findOne({ id: req.params.gameId });
     res.sendStatus(game.settings);
