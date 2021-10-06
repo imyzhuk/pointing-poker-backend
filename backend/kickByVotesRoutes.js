937aa96323ff90bdca6d9bc2adec3ac0f9691371
Kate <kate.matveeva@gmail.com>
2021-10-06 20:47:31 +0300

    fix: fix  votes when kick member

--- backend/kickByVotesRoutes.js
+++ backend/kickByVotesRoutes.js
@@ -8,7 +8,6 @@ module.exports = function (getIoInstance) {
     try {
       const game = await Game.findOne({ id: req.params.gameId });
       if (action !== 'start kicking' && !game.kickVotes.find( el => playerToKickId === el.playerId )){
-        console.log('this vote was finished', playerToKickId)
         res.sendStatus(200)
         return
       }
@@ -20,25 +19,25 @@ module.exports = function (getIoInstance) {
       )
       if (action === 'start kicking') {
         getIoInstance().to(req.params.gameId).emit('kickPlayer', { playerToKickId, kickProposeById });
-        console.log('init voting', playerToKickId)
       } else {
         const yes = game.kickVotes.filter((el) => playerToKickId === el.playerId && el.vote).length;
         const no = game.kickVotes.filter((el) => playerToKickId === el.playerId && !el.vote).length;
         const memberNeeded = Math.ceil(game.members.length / 2);
-        console.log(yes, no, memberNeeded)
         if ( yes >= memberNeeded ) {
           const memberToKick = game.members.findIndex(
             (member) => member.id === playerToKickId
           );
           game.members.splice(memberToKick, 1);
           getIoInstance().to(req.params.gameId).emit('membersChange', game.members);
+          game.votes = game.votes.forEach((vote) => {
+            const voteToDelete = vote.score.findIndex(s => s.playerId === playerToKickId);
+            if (voteToDelete !== -1) {
+              vote.score.splice(voteToDelete, 1);
+            }
+          });
         }
-        console.log((yes >= memberNeeded) , (no >= memberNeeded) , (yes + no === game.members.length));
         if ((yes >= memberNeeded) || (no >= memberNeeded) || (yes + no === game.members.length)){
-          console.log(game.kickVotes.length);
-          console.log(game.kickVotes.map(v => `${v.playerId} - ${v.vote}`));
           game.kickVotes = game.kickVotes.filter((el) => el.playerId !== playerToKickId);
-          console.log(game.kickVotes.length);
         }
       }
       await game.save();
