1ec65f8772699001e4d9b8ba4cece43cacc8ed20
Kate <kate.matveeva@gmail.com>
2021-10-06 22:40:56 +0300

    fix: fix  kick member and delete vote

--- backend/kickByVotesRoutes.js
+++ backend/kickByVotesRoutes.js
@@ -17,30 +17,29 @@ module.exports = function (getIoInstance) {
       game.kickVotes.push(
         { playerId: playerToKickId, action, vote }
       )
+      await game.save();
+
       if (action === 'start kicking') {
         getIoInstance().to(req.params.gameId).emit('kickPlayer', { playerToKickId, kickProposeById });
       } else {
         const yes = game.kickVotes.filter((el) => playerToKickId === el.playerId && el.vote).length;
-        const no = game.kickVotes.filter((el) => playerToKickId === el.playerId && !el.vote).length;
         const memberNeeded = Math.ceil(game.members.length / 2);
         if ( yes >= memberNeeded ) {
-          const memberToKick = game.members.findIndex(
-            (member) => member.id === playerToKickId
-          );
-          game.members.splice(memberToKick, 1);
-          getIoInstance().to(req.params.gameId).emit('membersChange', game.members);
-          game.votes = game.votes.forEach((vote) => {
-            const voteToDelete = vote.score.findIndex(s => s.playerId === playerToKickId);
-            if (voteToDelete !== -1) {
-              vote.score.splice(voteToDelete, 1);
-            }
-          });
-        }
-        if ((yes >= memberNeeded) || (no >= memberNeeded) || (yes + no === game.members.length)){
-          game.kickVotes = game.kickVotes.filter((el) => el.playerId !== playerToKickId);
+          const gameUpdated = await Game.findOneAndUpdate(
+            { id: req.params.gameId },
+            {
+              $pull: {
+                "votes.$[].score": { playerId:playerToKickId },
+                "members": { id: playerToKickId}
+              }
+            },
+            { multi: true, new: true }
+          )
+          getIoInstance().to(req.params.gameId).emit('membersChange', gameUpdated.members);
+          getIoInstance().to(req.params.gameId).emit('roundResultChange', gameUpdated.votes.find(task => task.taskId === game.currentTaskId));
+
         }
       }
-      await game.save();
       res.sendStatus(200);
     } catch (e) {
       console.log(e);
